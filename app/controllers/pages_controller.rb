1f1743da2d1a51a77962b8da0ff078e1a39f4c12
Luis Borges <borgesluisj@gmail.com>
2020-06-17 11:55:59 +0200

    added pages ans styling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,21 @@
 class PagesController < ApplicationController
+
   def home
   end
+
+  def deutsch
+  end
+
+  def espanol
+  end
+
+  def english
+  end
+
+  def imprint
+  end
+
+  def privacy
+  end
+
 end
