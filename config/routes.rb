54b5547c2ea4d3792f15b9239bc0d91754a38580
Luis Borges <borgesluisj@gmail.com>
2020-06-19 12:08:07 +0200

    footer links functioning

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home_en'
   get "espanol", to: "pages#home_es"
   get "english", to: "pages#home_en"
+  get "imprint", to: "pages#imprint"
 
   resources :subscribers
 
