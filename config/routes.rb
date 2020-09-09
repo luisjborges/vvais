2427c38b7df5dc04923fc8850271cb0ec08f9ebf
Luis Borges <borgesluisj@gmail.com>
2020-09-09 08:18:49 +0200

    vvais 2.0

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
   get "imprint", to: "pages#imprint"
   get "pf", to: "pages#pf"
   get "faq", to: "pages#faq"
+  get "rastreador_financiero", to: "pages#rastreador_financiero"
+  get "finance_tracker", to: "pages#finance_tracker"
+  get "becarios", to: "pages#becarios"
+  get "scholars", to: "pages#scholars"
+
 
   resources :subscribers
 
